7af811578dc618a51ca5e4c0b4122dd608834ae2
Uncle Scientist <unclescientist@gmail.com>
2025-08-25 08:17:40 -0400

    KoA 2024 Quest 9 Part 3

--- solutions/day09/src/main.rs
+++ solutions/day09/src/main.rs
@@ -1,6 +1,8 @@
+use aoclib::CoinChange;
+
 fn main() {
     let brightnesses: Vec<usize> = aoclib::read_numbers("input/everybody_codes_e2024_q09_p1.txt");
-    let stamps = aoclib::CoinChange::new(&[1, 3, 5, 10]);
+    let mut stamps = CoinChange::new(&[1, 3, 5, 10]);
     println!(
         "part 1 = {}",
         brightnesses
@@ -10,7 +12,7 @@ fn main() {
     );
 
     let brightnesses: Vec<usize> = aoclib::read_numbers("input/everybody_codes_e2024_q09_p2.txt");
-    let stamps = aoclib::CoinChange::new(&[1, 3, 5, 10, 15, 16, 20, 24, 25, 30]);
+    let mut stamps = CoinChange::new(&[1, 3, 5, 10, 15, 16, 20, 24, 25, 30]);
     println!(
         "part 2 = {}",
         brightnesses
@@ -18,4 +20,44 @@ fn main() {
             .filter_map(|b| stamps.min_coins(*b))
             .sum::<usize>()
     );
+
+    let brightnesses: Vec<usize> = aoclib::read_numbers("input/everybody_codes_e2024_q09_p3.txt");
+    let mut stamps = CoinChange::new(&[
+        1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
+    ]);
+    let mut total = 0;
+    for b in brightnesses {
+        println!("{b}");
+        total += find_best_split(&mut stamps, b);
+    }
+    println!("part 3 = {total}");
+}
+
+fn find_best_split(cc: &mut CoinChange<usize>, value: usize) -> usize {
+    let mut best = usize::MAX;
+    let mut lower = value / 2;
+    let mut upper = value - lower;
+    while (upper - lower) <= 100 {
+        if let Some(u) = cc.min_coins(upper)
+            && let Some(l) = cc.min_coins(lower)
+        {
+            best = best.min(u + l);
+        }
+        lower -= 1;
+        upper = value - lower;
+    }
+    best
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_first_value() {
+        let mut stamps = CoinChange::new(&[
+            1, 3, 5, 10, 15, 16, 20, 24, 25, 30, 37, 38, 49, 50, 74, 75, 100, 101,
+        ]);
+        assert_eq!(775 + 775, find_best_split(&mut stamps, 156488));
+    }
 }
