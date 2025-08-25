c89106479b3759be4f5ec3dd3e2d4e223e66b1d4
Uncle Scientist <unclescientist@gmail.com>
2025-08-25 07:49:58 -0400

    KoA 2024 Quest 9 Part 2

--- solutions/day09/src/main.rs
+++ solutions/day09/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
     let brightnesses: Vec<usize> = aoclib::read_numbers("input/everybody_codes_e2024_q09_p1.txt");
-    let coins = aoclib::CoinChange::new(&[1, 3, 5, 10]);
+    let stamps = aoclib::CoinChange::new(&[1, 3, 5, 10]);
     println!(
         "part 1 = {}",
         brightnesses
             .iter()
-            .filter_map(|b| coins.min_coins(*b))
+            .filter_map(|b| stamps.min_coins(*b))
+            .sum::<usize>()
+    );
+
+    let brightnesses: Vec<usize> = aoclib::read_numbers("input/everybody_codes_e2024_q09_p2.txt");
+    let stamps = aoclib::CoinChange::new(&[1, 3, 5, 10, 15, 16, 20, 24, 25, 30]);
+    println!(
+        "part 2 = {}",
+        brightnesses
+            .iter()
+            .filter_map(|b| stamps.min_coins(*b))
             .sum::<usize>()
     );
 }
