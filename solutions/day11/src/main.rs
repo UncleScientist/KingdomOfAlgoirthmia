6b3445ff396408a37dd62a40fcbe31f5bd94f1bd
Uncle Scientist <unclescientist@gmail.com>
2025-09-12 07:23:59 -0400

    refactor

--- solutions/day11/src/main.rs
+++ solutions/day11/src/main.rs
@@ -2,24 +2,42 @@ use std::collections::HashMap;
 
 fn main() {
     let lines = aoclib::read_lines("input/everybody_codes_e2024_q11_p1.txt");
+    let termites = Termites::new(&lines);
 
-    let map = lines
-        .iter()
-        .map(|line| {
-            let (left, right) = line.split_once(':').unwrap();
-            let right = right.split(',').collect();
-            (left, right)
-        })
-        .collect::<HashMap<&str, Vec<&str>>>();
-    let mut generation = vec!["A"];
-    for _ in 0..4 {
-        let mut next_gen = Vec::<&str>::new();
-        for g in generation {
-            if let Some(entry) = map.get(&g) {
-                next_gen.extend(entry);
-            }
+    println!("part 1 = {}", termites.count("A", 4));
+
+    //let lines = aoclib::read_lines("input/everybody_codes_e2024_q11_p2.txt");
+}
+
+struct Termites<'a> {
+    map: HashMap<&'a str, Vec<&'a str>>,
+}
+
+impl<'a> Termites<'a> {
+    fn new(lines: &'a [String]) -> Self {
+        Self {
+            map: lines
+                .iter()
+                .map(|line| {
+                    let (left, right) = line.split_once(':').unwrap();
+                    let right = right.split(',').collect();
+                    (left, right)
+                })
+                .collect(),
         }
-        generation = next_gen;
     }
-    println!("part 1 = {}", generation.len());
+
+    fn count(&self, start: &str, generations: usize) -> usize {
+        let mut generation = vec![start];
+        for _ in 0..generations {
+            let mut next_gen = Vec::<&str>::new();
+            for g in generation {
+                if let Some(entry) = self.map.get(&g) {
+                    next_gen.extend(entry);
+                }
+            }
+            generation = next_gen;
+        }
+        generation.len()
+    }
 }
