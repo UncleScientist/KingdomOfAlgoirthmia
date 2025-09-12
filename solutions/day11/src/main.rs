852a95b6493670b4bd5f90767e7cbfaf8500b14d
Uncle Scientist <unclescientist@gmail.com>
2025-09-12 07:19:32 -0400

    KoA 2024 Quest 11 Part 1

--- solutions/day11/src/main.rs
+++ solutions/day11/src/main.rs
@@ -1,3 +1,25 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let lines = aoclib::read_lines("input/everybody_codes_e2024_q11_p1.txt");
+
+    let map = lines
+        .iter()
+        .map(|line| {
+            let (left, right) = line.split_once(':').unwrap();
+            let right = right.split(',').collect();
+            (left, right)
+        })
+        .collect::<HashMap<&str, Vec<&str>>>();
+    let mut generation = vec!["A"];
+    for _ in 0..4 {
+        let mut next_gen = Vec::<&str>::new();
+        for g in generation {
+            if let Some(entry) = map.get(&g) {
+                next_gen.extend(entry);
+            }
+        }
+        generation = next_gen;
+    }
+    println!("part 1 = {}", generation.len());
 }
