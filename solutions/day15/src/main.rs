109bbf4c2549305bbb40eba38aa01dc50c87a9c1
Uncle Scientist <unclescientist@gmail.com>
2025-10-02 07:29:42 -0400

    refactor part 1 to get ready for part 2

--- solutions/day15/src/main.rs
+++ solutions/day15/src/main.rs
@@ -1,42 +1,60 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn main() {
     let data = std::fs::read_to_string("input/everybody_codes_e2024_q15_p1.txt").expect("file");
+    let garden = Garden::parse(&data);
+    println!("part 1 = {}", garden.search_any().unwrap() * 2);
 
-    let mut maze = HashSet::<(i64, i64)>::new();
-    let mut herbs = HashSet::<(i64, i64)>::new();
-    let mut start = (0, 0);
+    //let data = std::fs::read_to_string("input/everybody_codes_e2024_q15_p2.txt").expect("file");
+    let data = std::fs::read_to_string("input/test_2.txt").expect("file");
+    let garden = Garden::parse(&data);
+    println!("part 2 = {}", garden.search_all().unwrap());
+}
 
-    for (row, line) in data.lines().enumerate() {
-        for (col, ch) in line.chars().enumerate() {
-            let loc = (row as i64, col as i64);
-            match ch {
-                '#' => {}
-                '.' => {
-                    maze.insert(loc);
-                    if row == 0 {
-                        start = loc;
+struct Garden {
+    maze: HashSet<(i64, i64)>,
+    herbs: HashMap<(i64, i64), char>,
+    start: (i64, i64),
+}
+
+impl Garden {
+    fn parse(data: &str) -> Self {
+        let mut maze = HashSet::<(i64, i64)>::new();
+        let mut herbs = HashMap::<(i64, i64), char>::new();
+        let mut start = (0, 0);
+
+        for (row, line) in data.lines().enumerate() {
+            for (col, ch) in line.chars().enumerate() {
+                let loc = (row as i64, col as i64);
+                match ch {
+                    '#' | '~' => {}
+                    '.' => {
+                        maze.insert(loc);
+                        if row == 0 {
+                            start = loc;
+                        }
                     }
+                    'A'..='Z' => {
+                        maze.insert(loc);
+                        herbs.insert(loc, ch);
+                    }
+                    _ => panic!("invalid character '{ch}'"),
                 }
-                'H' => {
-                    maze.insert(loc);
-                    herbs.insert(loc);
-                }
-                _ => panic!("invalid character '{ch}'"),
             }
         }
+
+        Self { maze, herbs, start }
     }
 
-    println!(
-        "part 1 = {}",
+    fn search_any(&self) -> Option<usize> {
         search::bfs(
-            &start,
+            &self.start,
             |pos| {
                 [(-1, 0), (1, 0), (0, -1), (0, 1)]
                     .iter()
                     .filter_map(|delta| {
                         let next = (pos.0 + delta.0, pos.1 + delta.1);
-                        if maze.contains(&next) {
+                        if self.maze.contains(&next) {
                             Some(next)
                         } else {
                             None
@@ -44,11 +62,13 @@ fn main() {
                     })
                     .collect()
             },
-            |pos| herbs.contains(pos)
+            |pos| self.herbs.contains_key(pos),
         )
-        .unwrap()
-            * 2
-    );
+    }
+
+    fn search_all(&self) -> Option<usize> {
+        None
+    }
 }
 
 mod search {
