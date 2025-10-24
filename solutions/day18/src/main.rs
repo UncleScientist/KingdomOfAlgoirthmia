80465497eee384bde2c1144ae52ff27d6cef9636
Uncle Scientist <unclescientist@gmail.com>
2025-10-24 07:03:07 -0400

    KoA 2024 Quest 18 Part 3

--- solutions/day18/src/main.rs
+++ solutions/day18/src/main.rs
@@ -1,10 +1,17 @@
-use std::{collections::HashSet, path::Path};
+use std::{
+    collections::{HashMap, HashSet},
+    path::Path,
+};
 
 fn main() {
     let mut garden = Garden::read_from_file("input/everybody_codes_e2024_q18_p1.txt");
     println!("part 1 = {}", garden.time_to_water_trees());
     let mut garden = Garden::read_from_file("input/everybody_codes_e2024_q18_p2.txt");
     println!("part 2 = {}", garden.time_to_water_trees());
+
+    let garden = Garden::read_from_file("input/everybody_codes_e2024_q18_p3.txt");
+    // let garden = Garden::read_from_file("input/test_3.txt");
+    println!("palm tree count = {}", garden.find_optimal_spot());
 }
 
 struct Garden {
@@ -48,12 +55,12 @@ impl Garden {
     }
 
     fn time_to_water_trees(&mut self) -> usize {
-        let mut queue = HashSet::from_iter(self.start.clone());
+        let mut step = HashSet::from_iter(self.start.clone());
         let mut visited = HashSet::new();
         let mut time = 0;
         while !self.palms.is_empty() {
             let mut next_step = HashSet::new();
-            for pos in queue {
+            for pos in step {
                 if visited.insert(pos) {
                     self.palms.remove(&pos);
                     for delta in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
@@ -64,12 +71,51 @@ impl Garden {
                     }
                 }
             }
-            queue = next_step;
+            step = next_step;
             if !self.palms.is_empty() {
                 time += 1;
             }
         }
 
         time
+    }
+
+    fn find_optimal_spot(&self) -> usize {
+        let mut found = HashMap::<(i64, i64), usize>::new();
+        let mut step = HashSet::<((i64, i64), (i64, i64))>::from_iter(
+            self.palms.iter().map(|palm| (*palm, *palm)),
+        );
+        let mut visited = HashSet::new();
+        let mut time = 0;
+        loop {
+            let mut next_step = HashSet::new();
+
+            for (palm, pos) in step {
+                if visited.insert((palm, pos)) {
+                    *found.entry(pos).or_default() += time;
+
+                    for delta in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+                        let newpos = (pos.0 + delta.0, pos.1 + delta.1);
+                        if self.maze.contains(&newpos) {
+                            next_step.insert((palm, newpos));
+                        }
+                    }
+                }
+            }
+
+            if next_step.is_empty() {
+                break;
+            }
+
+            time += 1;
+            step = next_step;
+        }
+
+        let mut min = usize::MAX;
+        for (_, &amount) in found.iter().filter(|(pos, _)| !self.palms.contains(pos)) {
+            min = min.min(amount);
+        }
+
+        min
     }
 }
