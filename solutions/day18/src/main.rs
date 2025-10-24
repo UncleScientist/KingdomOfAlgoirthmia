5af546e9e1aeec67980ce3b05461029ee648d0a3
Uncle Scientist <unclescientist@gmail.com>
2025-10-24 06:28:19 -0400

    KoA 2024 Quest 18 Part 1

--- solutions/day18/src/main.rs
+++ solutions/day18/src/main.rs
@@ -1,3 +1,60 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let lines = aoclib::read_lines("input/everybody_codes_e2024_q18_p1.txt");
+    // let lines = aoclib::read_lines("input/test_1.txt");
+
+    let mut maze = HashSet::<(i64, i64)>::new();
+    let mut palms = HashSet::<(i64, i64)>::new();
+    let mut start = None;
+
+    for (row, line) in lines.iter().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            let (row, col) = (row as i64, col as i64);
+            match ch {
+                '#' => {}
+                '.' => {
+                    maze.insert((row, col));
+                    if col == 0 {
+                        start = Some((row, col));
+                    }
+                }
+                'P' => {
+                    maze.insert((row, col));
+                    palms.insert((row, col));
+                }
+                _ => {
+                    panic!("invalid char '{ch}'");
+                }
+            }
+        }
+    }
+
+    let Some(start) = start else {
+        panic!("starting location not found");
+    };
+
+    let mut queue = HashSet::from([start]);
+    let mut visited = HashSet::new();
+    let mut time = 0;
+    while !palms.is_empty() {
+        let mut next_step = HashSet::new();
+        for pos in queue {
+            if visited.insert(pos) {
+                palms.remove(&pos);
+                for delta in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+                    let newpos = (pos.0 + delta.0, pos.1 + delta.1);
+                    if maze.contains(&newpos) {
+                        next_step.insert(newpos);
+                    }
+                }
+            }
+        }
+        queue = next_step;
+        if !palms.is_empty() {
+            time += 1;
+        }
+    }
+
+    println!("part 1 = {time}");
 }
