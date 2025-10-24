e77d90182e08b2b2978c7f19916cc08fa2868f60
Uncle Scientist <unclescientist@gmail.com>
2025-10-24 06:07:41 -0400

    Fixed my bug accessing the binaryheap results

--- solutions/day17/src/main.rs
+++ solutions/day17/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     cmp::Reverse,
-    collections::{BinaryHeap, HashMap, HashSet},
+    collections::{BinaryHeap, HashSet},
 };
 
 fn main() {
@@ -13,48 +13,19 @@ fn main() {
     println!("part 2 = {}", star_count + prim(&edge_list).1);
 
     let lines = aoclib::read_lines("input/everybody_codes_e2024_q17_p3.txt");
-    // let lines = aoclib::read_lines("input/test_3.txt");
     let (_, mut edge_list) = generate_edges(&lines, 6);
 
-    let mut stars: Vec<(usize, usize)> = Vec::new();
-    for (row, line) in lines.iter().enumerate() {
-        for (col, ch) in line.chars().enumerate() {
-            if ch == '*' {
-                stars.push((row, col));
-            }
-        }
-    }
-    let mut starmap = HashMap::<(usize, usize), char>::new();
-    let mut charid = b'!';
-
     let mut constellations = BinaryHeap::new();
-    let mut count = 0;
     while !edge_list.is_empty() {
         let (found, distance) = prim(&edge_list);
-        for star in &found {
-            starmap.insert(stars[*star], charid as char);
-        }
-        charid += 1;
         constellations.push(distance + found.len());
         edge_list.retain(|(_, i, j)| !found.contains(i) && !found.contains(j));
-        count += 1;
     }
 
-    for row in 0..lines.len() {
-        for col in 0..lines[0].len() {
-            if let Some(ch) = starmap.get(&(row, col)) {
-                print!("{ch}");
-            } else {
-                print!(" ");
-            }
-        }
-        println!();
-    }
-
-    println!(
-        "part 3 = {} ({count})",
-        constellations.iter().take(3).product::<usize>()
-    );
+    let part3 = constellations.pop().unwrap()
+        * constellations.pop().unwrap()
+        * constellations.pop().unwrap();
+    println!("part 3 = {part3}",);
 }
 
 fn generate_edges(lines: &[String], limit: usize) -> (usize, Vec<(usize, usize, usize)>) {
